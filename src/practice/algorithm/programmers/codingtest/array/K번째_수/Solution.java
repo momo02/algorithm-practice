bb4c8c43833e65b3cddbb39eced04c4300dcaf43
momo02 <jungha0902@gmail.com>
2021-05-17 11:56:28 +0900

    코딩테스트.정렬.K번째 수(Arrays.copyOfRange 함수 사용하지않고)

--- src/practice/algorithm/programmers/codingtest/array/K번째_수/Solution.java
+++ src/practice/algorithm/programmers/codingtest/array/K번째_수/Solution.java
@@ -4,6 +4,45 @@ import java.util.Arrays;
 
 public class Solution {
 
+    /**
+     * Arrays.copyOfRange 함수 사용하지 않고 구현.
+     *
+     * 수행 시간 : 1022221ns(나노타임/세크)
+     * 수행 시간 : 599184ns(나노타임/세크)
+     * 수행 시간 : 961552ns(나노타임/세크)
+     * 수행 시간 : 874044ns(나노타임/세크)
+     * 수행 시간 : 1182645ns(나노타임/세크)
+     */
+    public int[] solution2(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++){
+            int[] c = new int[commands[i][1] - commands[i][0] + 1];
+
+            int j = 0;
+
+            for(int k=0; k<array.length; k++){
+                if(k >= commands[i][0]-1 && k < commands[i][1]){
+                    c[j++] = array[k];
+                }
+            }
+
+            Arrays.sort(c);
+            answer[i] = c[commands[i][2]-1];
+        }
+        return answer;
+    }
+
+
+    /**
+     * Arrays.copyOfRange 함수 사용
+     *
+     * 수행 시간 : 222988ns(나노타임/세크)
+     * 수행 시간 : 343527ns(나노타임/세크)
+     * 수행 시간 : 269829ns(나노타임/세크)
+     * 수행 시간 : 251365ns(나노타임/세크)
+     * 수행 시간 : 290685ns(나노타임/세크)
+     */
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
 
@@ -25,7 +64,10 @@ public class Solution {
         };
         int[] answer = {5, 6, 3};
 
-        int[] result = s.solution(array, commands);
+        long startTime = System.nanoTime();
+        int[] result = s.solution2(array, commands);
+        long endTime = System.nanoTime();
+
         for(int i=0; i<result.length; i++) {
             if(answer[i] == result[i]) {
                 System.out.println((i+1) + ". 정답!");
@@ -33,5 +75,8 @@ public class Solution {
                 System.out.println((i+1) + ". 오답!");
             }
         }
+        long resultTime = (endTime - startTime);
+        System.out.println("수행 시간 : " + resultTime +"ns(나노타임/세크)");
     }
+
 }
