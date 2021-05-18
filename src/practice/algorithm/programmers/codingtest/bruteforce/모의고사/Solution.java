15652b512634572140001840cdd4a88503acf000
momo02 <jungha0902@gmail.com>
2021-05-19 01:03:04 +0900

    코딩테스트.완전탐색.모의고사-첫번째 풀이

--- src/practice/algorithm/programmers/codingtest/bruteforce/모의고사/Solution.java
+++ src/practice/algorithm/programmers/codingtest/bruteforce/모의고사/Solution.java
@@ -5,8 +5,6 @@ import java.util.Arrays;
 public class Solution {
 
     public int[] solution(int[] answers) {
-        int[] answer = {};
-
         int[] p1 = {1,2,3,4,5};
         int[] p2 = {2,1,2,3,2,4,2,5};
         int[] p3 = {3,3,1,1,2,2,4,4,5,5};
@@ -19,13 +17,44 @@ public class Solution {
 //        System.out.println(b);
 //        System.out.println(c);
 
-        return answer;
+        if(a > b){
+            if(a > c){
+                return new int[]{1};
+            }else if(a == c){
+                return new int[]{1,3};
+            }else {
+                return new int[]{3};
+            }
+        }else if(a == b){
+            if(a > c) {
+                return new int[]{1,2};
+            }else if(a == c){
+                return new int[]{1,2,3};
+            }else {
+                return new int[]{3};
+            }
+        }else { //a <b
+            if(b > c) {
+                return new int[]{2};
+            }else if(b == c){
+                return new int[]{2,3};
+            }else {
+                return new int[]{3};
+            }
+        }
     }
 
     public int getGrade(int[] p, int[] answers){
         int grade = 0;
         for(int i=0; i<answers.length; i++){
-            if( p[i] == answers[i] ) {
+//            int pIdx;
+//            if(i >= p.length) {
+//                pIdx = i % p.length;
+//            }else{
+//                pIdx = i;
+//            }
+//            System.out.println(" >>" + pIdx);
+            if( p[i % p.length] == answers[i] ) {
                 grade++;
             }
         }
@@ -36,16 +65,20 @@ public class Solution {
         Solution s = new Solution();
         int[][] commands = {
                 {1,2,3,4,5},
-                {1,3,2,4,2}
+                {1,3,2,4,2},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
         };
         int[][] returns = {
                 {1},
+                {1,2,3},
+                {3},
                 {1,2,3}
         };
 
         long startTime = System.nanoTime();
         for(int i=0; i<commands.length; i++){
-          if(returns[i] ==  s.solution(commands[i])){
+          if(Arrays.equals(returns[i], s.solution(commands[i]))){
               System.out.println((i+1) + ". 정답!");
             }else{
               System.out.println((i+1) + ". 오답!");
